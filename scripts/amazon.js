689f72e97f854a6c22b2714bb48da063b6e17f0f
PHAN DANG PHUC <phandangphuc812@gmail.com>
2024-07-17 16:39:12 +0900

    Fix error in showing star number

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
             <img class="product-rating-stars"
                 src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-                ${product.reviewCount}
+                ${product.rating.reviewCount}
             </div>
             </div>
 
