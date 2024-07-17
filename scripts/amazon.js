be5b4ffcb12c5a17b1331117d810b433cec749a9
PHAN DANG PHUC <phandangphuc812@gmail.com>
2024-07-17 17:01:36 +0900

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
 // Array: Represent a list of element
 //        Each element has many properties
 // Object: group multiple properties/value together
-const products = [{
-    imageSrc: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        reviewCount: 87
-    },
-    priceCents: 1090
-}, {
-    imageSrc: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        reviewCount: 127
-    },
-    priceCents: 2095
-}, {
-    imageSrc: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        reviewCount: 56
-    },
-    priceCents: 799
-}];
-
 let productsHTML = '';
 
 // Take each product and store at (object)product and run the function
@@ -35,7 +9,7 @@ products.forEach((product) => {
         <div class="product-container">
             <div class="product-image-container">
             <img class="product-image"
-                src="${product.imageSrc}">
+                src="${product.image}">
             </div>
 
             <div class="product-name limit-text-to-2-lines">
@@ -46,7 +20,7 @@ products.forEach((product) => {
             <img class="product-rating-stars"
                 src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-                ${product.rating.reviewCount}
+                ${product.rating.count}
             </div>
             </div>
 
